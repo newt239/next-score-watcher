361c93c34db3b29f39a1c10d9510dc08d810ddd0
newt239 <ysti2019.win2020.st@gmail.com>
2023-01-21 13:13:56 +0900

    feature: delete app data button

--- src/pages/option/index.tsx
+++ src/pages/option/index.tsx
@@ -1,20 +1,45 @@
 import { NextPageWithLayout } from "next";
 import Head from "next/head";
+import router from "next/router";
+import React, { useRef } from "react";
 
 import {
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogOverlay,
+  Box,
+  Button,
   Container,
+  Flex,
   FormControl,
+  FormHelperText,
   FormLabel,
   Stack,
   Switch,
   useColorMode,
+  useDisclosure,
 } from "@chakra-ui/react";
 
 import H2 from "#/blocks/H2";
 import { Layout } from "#/layouts/Layout";
+import db from "#/utils/db";
 
 const OptionPage: NextPageWithLayout = () => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const latestVersion = process.env.NEXT_PUBLIC_APP_VERSION;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const cancelRef = useRef();
+
+  const deleteAppData = () => {
+    localStorage.setItem("VERSION", latestVersion!);
+    db.delete().then(() => {
+      router.reload();
+    });
+  };
+
   return (
     <>
       <Head>
@@ -30,7 +55,7 @@ const OptionPage: NextPageWithLayout = () => {
               justifyContent: "space-between",
             }}
           >
-            <FormLabel htmlFor="color-mode" mb="0">
+            <FormLabel htmlFor="color-mode" sx={{ flexGrow: 1 }}>
               ダークモード
             </FormLabel>
             <Switch
@@ -40,8 +65,52 @@ const OptionPage: NextPageWithLayout = () => {
               onChange={() => toggleColorMode()}
             />
           </FormControl>
+          <FormControl>
+            <Flex
+              sx={{
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <Box>
+                <FormLabel sx={{ flexGrow: 1 }}>アプリの初期化</FormLabel>
+                <FormHelperText>
+                  アプリが上手く動作しない場合にお試しください。
+                </FormHelperText>
+              </Box>
+              <Button colorScheme="red" onClick={onOpen}>
+                初期化する
+              </Button>
+            </Flex>
+          </FormControl>
         </Stack>
       </Container>
+      <AlertDialog
+        isOpen={isOpen}
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+      >
+        <AlertDialogOverlay>
+          <AlertDialogContent>
+            <AlertDialogHeader fontSize="lg" fontWeight="bold">
+              アプリを初期化します
+            </AlertDialogHeader>
+
+            <AlertDialogBody>
+              この操作は取り消せません。本当に初期化してよろしいですか？
+            </AlertDialogBody>
+
+            <AlertDialogFooter>
+              <Button ref={cancelRef} onClick={onClose}>
+                やめる
+              </Button>
+              <Button colorScheme="red" onClick={deleteAppData} ml={3}>
+                初期化する
+              </Button>
+            </AlertDialogFooter>
+          </AlertDialogContent>
+        </AlertDialogOverlay>
+      </AlertDialog>
     </>
   );
 };
