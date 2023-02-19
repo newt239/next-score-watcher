7678ef38862f6fddc379b86dbd098130f2a6e375
newt239 <ysti2019.win2020.st@gmail.com>
2023-02-19 20:27:39 +0900

    change: move to top page Term

--- src/pages/option/index.tsx
+++ src/pages/option/index.tsx
@@ -221,18 +221,6 @@ const OptionPage: NextPageWithLayout = () => {
           </AlertDialogContent>
         </AlertDialogOverlay>
       </AlertDialog>
-      <H2>本ソフトのご利用にあたって</H2>
-      <UnorderedList py={5}>
-        <ListItem>
-          本ソフトはあくまで「得点表示ソフト」であり、問題やプレイヤー、試合記録の管理ソフトではありません。
-        </ListItem>
-        <ListItem>
-          データはすべて端末上に保存されますが、アップデートにより予告なくデータがリセットされることがあります。
-        </ListItem>
-        <ListItem>
-          本ソフトの開発者は本ソフトを使用したことにより生じる損害について、いかなる責任も負いません。
-        </ListItem>
-      </UnorderedList>
       <H2>お問い合わせ</H2>
       <Box py={5}>
         <Text>
