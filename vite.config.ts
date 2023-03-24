42bdf4b7936d74cc0e6fe8a7dcaff7740257beb5
newt239 <ysti2019.win2020.st@gmail.com>
2023-03-25 01:15:35 +0900

    add: pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { visualizer } from "rollup-plugin-visualizer";
+import { VitePWA } from "vite-plugin-pwa";
 import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [react()],
+    plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
     resolve: {
       alias: {
         "#": path.resolve(__dirname, "src"),
