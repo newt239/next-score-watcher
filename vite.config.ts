068ed28f9797cd5d26d862ca8bf9a0c0cb7a5af0
newt239 <ysti2019.win2020.st@gmail.com>
2023-03-25 01:04:10 +0900

    chore: add analyze option

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { visualizer } from "rollup-plugin-visualizer";
 import * as path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "#": path.resolve(__dirname, "src"),
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "#": path.resolve(__dirname, "src"),
+      },
     },
-  },
+    build: {
+      rollupOptions: {
+        plugins: [
+          mode === "analyze" &&
+            visualizer({
+              open: true,
+              filename: "dist/stats.html",
+              gzipSize: true,
+              brotliSize: true,
+            }),
+        ],
+      },
+    },
+  };
 });
