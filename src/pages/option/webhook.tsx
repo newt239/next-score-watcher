c86c8da8a3fe258f0bf747a26a2dbe20c5d4f566
newt239 <ysti2019.win2020.st@gmail.com>
2023-05-13 21:05:30 +0900

    improve: webhook page description

--- src/pages/option/webhook.tsx
+++ src/pages/option/webhook.tsx
@@ -1,8 +1,16 @@
 import { useEffect } from "react";
 import { Link as ReactLink } from "react-router-dom";
 
-import { Box, Button, Code, Container, Text } from "@chakra-ui/react";
-import { ArrowBackUp } from "tabler-icons-react";
+import {
+  Box,
+  Button,
+  Code,
+  Container,
+  Icon,
+  Link,
+  Text,
+} from "@chakra-ui/react";
+import { ArrowBackUp, ExternalLink } from "tabler-icons-react";
 
 const WebhookPage = () => {
   useEffect(() => {
@@ -31,6 +39,17 @@ const WebhookPage = () => {
         <Code>scores</Code>で構成されています。
       </Text>
       <Text>詳細は実際にご利用の上お確かめください。</Text>
+      <h3>Response Example</h3>
+      <Link
+        href="https://gist.github.com/newt239/e03f8ed7fbc00852999bced29d80e8af"
+        isExternal
+        color="blue.500"
+      >
+        Webhookで提供されるjsonの例｜Score Watcher
+        <Icon>
+          <ExternalLink />
+        </Icon>
+      </Link>
     </Container>
   );
 };
