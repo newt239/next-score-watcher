1f732ec12ec0a372dd5186febe135f721816a094
newt239 <ysti2019.win2020.st@gmail.com>
2023-08-08 23:14:19 +0900

    improve: the design of game list

--- src/components/home/GameList.tsx
+++ src/components/home/GameList.tsx
@@ -4,34 +4,17 @@ import { Link as ReactLink } from "react-router-dom";
 import {
   Box,
   Button,
+  Card,
   Flex,
-  HStack,
-  IconButton,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuList,
   Select,
-  Table,
-  TableContainer,
-  Tbody,
-  Td,
-  Th,
-  Thead,
-  Tr,
+  SimpleGrid,
+  Text,
 } from "@chakra-ui/react";
 import { cdate } from "cdate";
 import { useLiveQuery } from "dexie-react-hooks";
-import {
-  AdjustmentsHorizontal,
-  Chalkboard,
-  Copy,
-  DotsCircleHorizontal,
-  Trash,
-} from "tabler-icons-react";
+import { AdjustmentsHorizontal } from "tabler-icons-react";
 
 import db from "#/utils/db";
-import { createGame } from "#/utils/functions";
 import { getRuleStringByType } from "#/utils/rules";
 
 const GameList: React.FC = () => {
@@ -44,6 +27,34 @@ const GameList: React.FC = () => {
 
   if (!games || games.length === 0) return null;
 
+  const parsedGameList = games
+    .sort((prev, cur) => {
+      if (orderType === "last_open") {
+        if (prev.last_open > cur.last_open) return -1;
+        if (prev.last_open < cur.last_open) return 1;
+        return 0;
+      } else {
+        if (prev.name < cur.name) return -1;
+        if (prev.name > cur.name) return 1;
+        return 0;
+      }
+    })
+    .map((game) => {
+      const eachGameLogs = (logs || []).filter(
+        (log) => log.game_id === game.id
+      );
+      const gameState =
+        eachGameLogs.length === 0 ? "設定中" : `${eachGameLogs.length}問目`;
+      return {
+        id: game.id,
+        name: game.name,
+        type: getRuleStringByType(game),
+        player_count: game.players.length,
+        state: gameState,
+        last_open: game.last_open,
+      };
+    });
+
   return (
     <Box pt={5}>
       <h2>作成したゲーム</h2>
@@ -54,106 +65,48 @@ const GameList: React.FC = () => {
           width={200}
         >
           <option value="last_open">最終閲覧順</option>
-          <option value="name">ラウンド名順</option>
+          <option value="name">ゲーム名順</option>
         </Select>
       </Flex>
-      <TableContainer pt={5}>
-        <Table size="sm" variant="simple">
-          <Thead>
-            <Tr>
-              <Th>ラウンド名</Th>
-              <Th>形式</Th>
-              <Th>人数</Th>
-              <Th>進行状況</Th>
-              <Th>最終閲覧</Th>
-              <Th></Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {games
-              .sort((prev, cur) => {
-                if (orderType === "last_open") {
-                  if (prev.last_open > cur.last_open) return -1;
-                  if (prev.last_open < cur.last_open) return 1;
-                  return 0;
-                } else {
-                  if (prev.name < cur.name) return -1;
-                  if (prev.name > cur.name) return 1;
-                  return 0;
-                }
-              })
-              .map((game) => {
-                const eachGameLogs = (logs || []).filter(
-                  (log) => log.game_id === game.id
-                );
-                const gameState =
-                  eachGameLogs.length === 0
-                    ? "設定中"
-                    : `${eachGameLogs.length}問目`;
-                return (
-                  <Tr key={game.id}>
-                    <Td>{game.name}</Td>
-                    <Td>{getRuleStringByType(game)}</Td>
-                    <Td>{game.players.length}</Td>
-                    <Td>{gameState}</Td>
-                    <Td>{cdate(game.last_open).format("MM/DD HH:mm")}</Td>
-                    <Td sx={{ textAlign: "right" }}>
-                      <HStack sx={{ justifyContent: "flex-end" }}>
-                        <Button
-                          as={ReactLink}
-                          colorScheme="green"
-                          leftIcon={<AdjustmentsHorizontal />}
-                          size="sm"
-                          to={`/${game.id}/config`}
-                          variant="ghost"
-                        >
-                          開く
-                        </Button>
-                        <Menu>
-                          <MenuButton
-                            aria-label="Options"
-                            as={IconButton}
-                            icon={<DotsCircleHorizontal />}
-                            size="sm"
-                            variant="ghost"
-                          />
-                          <MenuList>
-                            <MenuItem
-                              icon={<Copy />}
-                              onClick={() =>
-                                createGame({ game, action_type: "copy-all" })
-                              }
-                            >
-                              コピーを作成
-                            </MenuItem>
-                            {game.players.length !== 0 && (
-                              <MenuItem
-                                as={ReactLink}
-                                icon={<Chalkboard />}
-                                to={`/${game.id}/board`}
-                              >
-                                得点画面を開く
-                              </MenuItem>
-                            )}
-                            <MenuItem
-                              color="red.500"
-                              icon={<Trash />}
-                              onClick={async () =>
-                                await db.games.delete(game.id)
-                              }
-                            >
-                              ゲームを削除
-                            </MenuItem>
-                          </MenuList>
-                        </Menu>
-                      </HStack>
-                    </Td>
-                  </Tr>
-                );
-              })}
-          </Tbody>
-        </Table>
-      </TableContainer>
+      <SimpleGrid
+        pt={3}
+        spacing={3}
+        templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
+      >
+        {parsedGameList.map((game) => (
+          <Card
+            gap={3}
+            justifyContent="space-between"
+            key={game.id}
+            p={3}
+            variant="filled"
+          >
+            <Box>
+              <h3 style={{ whiteSpace: "nowrap", overflowX: "scroll" }}>
+                {game.name}
+              </h3>
+              <Text>
+                {game.type} ／ {game.player_count}人
+              </Text>
+              <Text>進行状況: {game.state}</Text>
+            </Box>
+            <Flex alignItems="center" justifyContent="space-between">
+              <Box sx={{ fontSize: "70%", opacity: 0.5 }}>
+                {cdate(game.last_open).format("MM/DD HH:mm")}
+              </Box>
+              <Button
+                as={ReactLink}
+                colorScheme="green"
+                leftIcon={<AdjustmentsHorizontal />}
+                size="sm"
+                to={`/${game.id}/config`}
+              >
+                開く
+              </Button>
+            </Flex>
+          </Card>
+        ))}
+      </SimpleGrid>
     </Box>
   );
 };
