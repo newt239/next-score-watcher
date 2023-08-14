4273444f2533e9863cb5d5907b8b313aead9a9a8
newt239 <ysti2019.win2020.st@gmail.com>
2023-08-14 21:17:57 +0900

    chore: set up Sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,36 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {
+  createRoutesFromChildren,
+  matchRoutes,
+  useLocation,
+  useNavigationType,
+} from "react-router-dom";
 
 import { ChakraProvider } from "@chakra-ui/react";
+import * as Sentry from "@sentry/react";
 
 import App from "#/App";
+
+Sentry.init({
+  dsn: import.meta.env.SENTRY_DSN,
+  integrations: [
+    new Sentry.BrowserTracing({
+      // See docs for support of different versions of variation of react router
+      // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+      routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+        React.useEffect,
+        useLocation,
+        useNavigationType,
+        createRoutesFromChildren,
+        matchRoutes
+      ),
+    }),
+    new Sentry.Replay(),
+  ],
+  environment: process.env.NODE_ENV,
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import * as path from "path";
 import { defineConfig } from "vite";
@@ -6,7 +7,18 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   return {
-    plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+    build: {
+      sourcemap: true, // Source map generation must be turned on
+    },
+    plugins: [
+      react(),
+      VitePWA({ registerType: "autoUpdate" }),
+      sentryVitePlugin({
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: "newt239",
+        project: "score-watcher",
+      }),
+    ],
     resolve: {
       alias: {
         "#": path.resolve(__dirname, "src"),
