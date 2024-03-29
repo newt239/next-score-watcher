63a27fb6f62345b7aff5f16bfc996dec9b566adf
newt239 <ysti2019.win2020.st@gmail.com>
2024-03-30 07:03:44 +0900

    update: v2.5.0

--- src/utils/features.tsx
+++ src/utils/features.tsx
@@ -1,6 +1,3 @@
-import { Icon, Link } from "@chakra-ui/react";
-import { ExternalLink } from "tabler-icons-react";
-
 type Feature = {
   news?: React.ReactNode;
   feature: string[];
@@ -8,24 +5,11 @@ type Feature = {
 };
 
 export const features: { [key: string]: Feature } = {
-  "2.4.5": {
-    news: (
-      <>
-        2024年3月までの期間、本サービスへのお問い合わせの受付を停止します。詳細は
-        <Link
-          color="blue.500"
-          href="https://newt-house.notion.site/Score-Watcher-Info-e3605dc670724bc8adf0a5ee3f0c8392"
-          isExternal
-        >
-          Notion上で公開しているページ
-          <Icon>
-            <ExternalLink />
-          </Icon>
-        </Link>
-        をご確認ください。
-      </>
-    ),
-    feature: [],
-    bugfix: ["個人設定が反映されない不具合を修正"],
+  "2.5.0": {
+    feature: [
+      "プロファイルの切り替え機能を追加: デスクトップ画面左下の「デフォルト」から切り替えることができます。",
+      "形式の追加: エンドレスチャンス",
+    ],
+    bugfix: [],
   },
 };
