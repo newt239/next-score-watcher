bf6fd1a2e0f7c771066b4ec6bebe3eb544665f96
newt239 <ysti2019.win2020.st@gmail.com>
2024-04-02 21:25:44 +0900

    update: v2.5.1

--- src/utils/features.tsx
+++ src/utils/features.tsx
@@ -5,10 +5,11 @@ type Feature = {
 };
 
 export const features: { [key: string]: Feature } = {
-  "2.5.0": {
+  "2.5.1": {
     feature: [
       "プロファイルの切り替え機能を追加: デスクトップ画面左下の「デフォルト」から切り替えることができます。",
       "形式の追加: エンドレスチャンス",
+      "形式の追加: Divide",
     ],
     bugfix: [],
   },
