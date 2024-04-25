5d5d253b044fba2b5da799db30604683d95622fc
newt239 <ysti2019.win2020.st@gmail.com>
2024-04-25 21:42:55 +0900

    fix: can't select existing rule players

--- src/features/config/PlayersConfig.tsx
+++ src/features/config/PlayersConfig.tsx
@@ -37,158 +37,124 @@ const PlayersConfig: React.FC<SelectPlayerProps> = ({
   return (
     <>
       <h2>プレイヤー設定</h2>
-      <Box>
-        <h3>プレイヤー選択</h3>
-        <SelectPlayerDrawer
-          disabled={disabled}
-          game_id={game_id}
-          playerList={playerList}
-          players={players}
-        />
-      </Box>
-      <Box>
-        <h3>並び替え</h3>
-        {players.length !== 0 ? (
-          <>
-            <Box
-              sx={{
-                mt: 3,
-                p: 3,
-                bgColor: "gray.300",
-                _dark: {
-                  bgColor: "gray.600",
-                },
-              }}
-            >
-              <ReactSortable
-                animation={200}
-                delay={2}
-                direction={isDesktop ? "vertical" : "horizontal"}
-                list={sortableList}
-                setList={(newState) => {
-                  (async () => {
-                    if (newState.length === players.length) {
-                      for (let i = 0; i < players.length; i++) {
-                        // 並び替えが行われたときのみ1回だけ処理する
-                        // 選択プレイヤーの変更と個人の初期値設定の変更のケースを除外
-                        if (players[i].id !== newState[i].id) {
-                          await db(currentProfile).games.update(game_id, {
-                            players: newState,
-                          });
-                          setSortableList(newState);
-                          break;
-                        }
+      <h3>プレイヤー選択</h3>
+      <SelectPlayerDrawer
+        disabled={disabled}
+        game_id={game_id}
+        playerList={playerList}
+        players={players}
+      />
+      {players.length !== 0 && (
+        <>
+          <h3>並び替え</h3>
+          <Box
+            sx={{
+              mt: 3,
+              p: 3,
+              bgColor: "gray.300",
+              _dark: {
+                bgColor: "gray.600",
+              },
+            }}
+          >
+            <ReactSortable
+              animation={200}
+              delay={2}
+              direction={isDesktop ? "vertical" : "horizontal"}
+              list={sortableList}
+              setList={(newState) => {
+                (async () => {
+                  if (newState.length === players.length) {
+                    for (let i = 0; i < players.length; i++) {
+                      // 並び替えが行われたときのみ1回だけ処理する
+                      // 選択プレイヤーの変更と個人の初期値設定の変更のケースを除外
+                      if (players[i].id !== newState[i].id) {
+                        await db(currentProfile).games.update(game_id, {
+                          players: newState,
+                        });
+                        setSortableList(newState);
+                        break;
                       }
                     }
-                  })();
-                }}
-                style={{
-                  display: "flex",
-                  flexDirection: isDesktop ? "row" : "column",
-                  gap: 5,
-                }}
-              >
-                {sortableList.map((player, index) => (
-                  <Card
-                    cursor="grab"
-                    key={player.id}
-                    sx={{
-                      bgColor: "gray.200",
-                      _dark: {
-                        bgColor: "gray.700",
-                      },
-                    }}
-                  >
-                    <CardBody>
+                  }
+                })();
+              }}
+              style={{
+                display: "flex",
+                flexDirection: isDesktop ? "row" : "column",
+                gap: 5,
+              }}
+            >
+              {sortableList.map((player, index) => (
+                <Card
+                  cursor="grab"
+                  key={player.id}
+                  sx={{
+                    bgColor: "gray.200",
+                    _dark: {
+                      bgColor: "gray.700",
+                    },
+                  }}
+                >
+                  <CardBody>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        flexDirection: isDesktop ? "column" : "row",
+                        gap: 3,
+                        justifyContent: "space-between",
+                        alignItems: "center",
+                        height: "100%",
+                      }}
+                    >
                       <Box
                         sx={{
-                          display: "flex",
-                          flexDirection: isDesktop ? "column" : "row",
-                          gap: 3,
-                          justifyContent: "space-between",
-                          alignItems: "center",
-                          height: "100%",
+                          writingMode: isDesktop
+                            ? "vertical-rl"
+                            : "horizontal-tb",
+                          whiteSpace: "nowrap",
+                          textOrientation: "upright",
                         }}
                       >
-                        <Box
-                          sx={{
-                            writingMode: isDesktop
-                              ? "vertical-rl"
-                              : "horizontal-tb",
-                            whiteSpace: "nowrap",
-                            textOrientation: "upright",
-                          }}
-                        >
-                          <p>{player.name}</p>
-                        </Box>
-                        {isDesktop && (
-                          <IndividualConfig
-                            correct={[
-                              "normal",
-                              "nomx",
-                              "nomx-ad",
-                              "ny",
-                              "nomr",
-                              "nbyn",
-                              "nupdown",
-                              "swedish10",
-                              "backstream",
-                              "variables",
-                              "attacksurvival",
-                            ].includes(rule_name)}
-                            disabled={disabled}
-                            index={index}
-                            wrong={[
-                              "nomx",
-                              "nomx-ad",
-                              "ny",
-                              "nomr",
-                              "nbyn",
-                              "nupdown",
-                              "swedish10",
-                              "backstream",
-                            ].includes(rule_name)}
-                          />
-                        )}
+                        <p>{player.name}</p>
                       </Box>
-                    </CardBody>
-                  </Card>
-                ))}
-              </ReactSortable>
-            </Box>
-            <p>
-              ※個人設定が行える形式では、個人の初期値を変更した場合、1問目の時点での勝ち抜けリーチや失格リーチが正しく表示されないことがあります。
-            </p>
-          </> // 上記はgetInitialPlayersStateでstateとreach_stateを共通でplayingにしていることによるもの
-        ) : (
-          <Box>ここに選択されたプレイヤーが表示されます</Box>
-        )}
-      </Box>
-      {!isDesktop && (
-        <Box>
-          <h3>個人設定</h3>
-          <ul>
-            {sortableList.map((player, index) => (
-              <li key={player.id}>
-                <span>{player.name}</span>
-                <IndividualConfig
-                  correct={[
-                    "normal",
-                    "nomx",
-                    "nomx-ad",
-                    "ny",
-                    "nomr",
-                    "variables",
-                    "attacksurvival",
-                  ].includes(rule_name)}
-                  disabled={disabled}
-                  index={index}
-                  wrong={["nomx", "nomx-ad", "ny", "nomr"].includes(rule_name)}
-                />
-              </li>
-            ))}
-          </ul>
-        </Box>
+                      <IndividualConfig
+                        correct={[
+                          "normal",
+                          "nomx",
+                          "nomx-ad",
+                          "ny",
+                          "nomr",
+                          "nbyn",
+                          "nupdown",
+                          "swedish10",
+                          "backstream",
+                          "variables",
+                          "attacksurvival",
+                        ].includes(rule_name)}
+                        disabled={disabled}
+                        index={index}
+                        wrong={[
+                          "nomx",
+                          "nomx-ad",
+                          "ny",
+                          "nomr",
+                          "nbyn",
+                          "nupdown",
+                          "swedish10",
+                          "backstream",
+                        ].includes(rule_name)}
+                      />
+                    </Box>
+                  </CardBody>
+                </Card>
+              ))}
+            </ReactSortable>
+          </Box>
+          <p>
+            ※個人設定が行える形式では、個人の初期値を変更した場合、1問目の時点での勝ち抜けリーチや失格リーチが正しく表示されないことがあります。
+          </p>
+        </> // 上記はgetInitialPlayersStateでstateとreach_stateを共通でplayingにしていることによるもの
       )}
     </>
   );
--- src/features/config/SelectPlayerFromExistingGame.tsx
+++ src/features/config/SelectPlayerFromExistingGame.tsx
@@ -1,4 +1,4 @@
-import { Box, Select, Text } from "@chakra-ui/react";
+import { Box, Select } from "@chakra-ui/react";
 import { useLiveQuery } from "dexie-react-hooks";
 import db from "~/utils/db";
 
@@ -15,32 +15,35 @@ const SelectPlayerFromExistingGame: React.FC<
   if (!games) return null;
 
   return (
-    <Box>
-      <Text>
+    <>
+      <p>
         これまでに作成したゲームで選択したプレイヤーをまとめて選択できます。
-      </Text>
-      <Select
-        onChange={async (e) => {
-          const selectedGame = games?.find(
-            (game) => game.id === e.target.value
-          );
-          if (selectedGame) {
-            await db(currentProfile).games.update(game_id, {
-              players: selectedGame.players,
-            });
-          }
-        }}
-      >
-        {games
-          .filter((game) => game.players.length !== 0)
-          .map((game) => (
-            <option key={game.id} value={game.id}>
-              {game.name} ({" "}
-              {game.players.map((player) => player.name).join(", ")} )
-            </option>
-          ))}
-      </Select>
-    </Box>
+      </p>
+      <Box mt={3}>
+        <Select
+          onChange={async (e) => {
+            const selectedGame = games?.find(
+              (game) => game.id === e.target.value
+            );
+            if (selectedGame) {
+              await db(currentProfile).games.update(game_id, {
+                players: selectedGame.players,
+              });
+            }
+          }}
+        >
+          <option value="">選択してください</option>
+          {games
+            .filter((game) => game.players.length !== 0)
+            .map((game) => (
+              <option key={game.id} value={game.id}>
+                {game.name} ({" "}
+                {game.players.map((player) => player.name).join(", ")} )
+              </option>
+            ))}
+        </Select>
+      </Box>
+    </>
   );
 };
 
