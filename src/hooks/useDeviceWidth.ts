00e9b13132747ce5971397ca8872d1c9263e1c0d
newt <ysti2019.win2020.st@gmail.com>
2024-06-06 14:46:08 +0900

    remove: vite components

--- src/hooks/useDeviceWidth.ts
+++
@@ -1,9 +0,0 @@
-import { useMediaQuery } from "@chakra-ui/react";
-
-const useDeviceWidth = (width?: number) => {
-  const [isDesktop] = useMediaQuery(`(min-width: ${width || 768}px)`);
-
-  return isDesktop;
-};
-
-export default useDeviceWidth;
