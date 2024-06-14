746ab7abfb0fcb49b781ca8e5be25939786f0b96
newt <ysti2019.win2020.st@gmail.com>
2024-06-14 23:41:21 +0900

    chore: remove unused packages

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
-import { createVanillaExtractPlugin } from "@vanilla-extract/next-plugin";
-
-const withVanillaExtract = createVanillaExtractPlugin();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 };
 
-export default withVanillaExtract(nextConfig);
+export default nextConfig;
