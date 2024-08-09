f178498eaf0da9560a150f656131eed2a3070f38
newt <ysti2019.win2020.st@gmail.com>
2024-08-09 15:48:54 +0900

    test: add playwright tests

--- tests/game.spec.ts
+++ tests/game.spec.ts
@@ -1,8 +1,41 @@
-import { expect, test } from "@playwright/test";
+import test, { expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:3000/");
-  await expect(page).toHaveTitle("Score Watcher");
-  await page.getByText("ゲームを作る").click();
-  await expect(page).toHaveTitle(/形式一覧/);
+});
+
+test.describe("アップデートモーダル", () => {
+  test.describe.configure({ mode: "default" });
+
+  test("初めてアクセスしたときアップデートモーダルが表示される", async ({
+    page,
+  }) => {
+    const headerEl = page.getByRole("dialog");
+    await expect(headerEl).toContainText(
+      "新しいバージョンがリリースされました"
+    );
+  });
+
+  test("アップデートモーダルを閉じる", async ({ page }) => {
+    await page.getByRole("dialog").locator("button").click();
+    const headerEl = page.getByRole("dialog");
+    await expect(headerEl).toBeVisible({ visible: false });
+  });
+});
+
+test.describe("得点表示", () => {
+  test.describe.configure({ mode: "default" });
+
+  test.beforeEach(async ({ page }) => {
+    await page.evaluate(() => {
+      // TODO: latest versionを.env.localから取得する
+      localStorage.setItem("scorewatcher-version", "3.0.2");
+    });
+    await page.reload();
+  });
+
+  test("形式一覧ページに移動できる", async ({ page }) => {
+    await page.getByRole("link", { name: "ゲームを作る" }).click();
+    await expect(page).toHaveTitle(/形式一覧/);
+  });
 });
