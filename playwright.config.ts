da6b3c6e031ea54b9824ca1803fe13bfa9e67a0f
newt <ysti2019.win2020.st@gmail.com>
2024-08-13 12:50:17 +0900

    fix: playwright test config

--- playwright.config.ts
+++ playwright.config.ts
@@ -36,7 +36,7 @@ export default defineConfig({
   ],
   webServer: {
     command: "pnpm run dev",
-    url: "http://localhost:3000",
+    port: 3000,
     reuseExistingServer: !process.env.CI,
   },
 });
