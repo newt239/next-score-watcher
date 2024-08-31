3f44beb60f685634b920c7cd070287ceec5a543f
newt <ysti2019.win2020.st@gmail.com>
2024-08-31 18:33:28 +0900

    add: show recommended environment

---
+++ src/app/(default)/docs/_components/RecommendedEnvironment.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { List, Table } from "@mantine/core";
+
+const RecommendedEnvironment = () => {
+  return (
+    <Table highlightOnHover mb="xl">
+      <Table.Tbody>
+        <Table.Tr>
+          <Table.Th>OS</Table.Th>
+          <Table.Td>
+            <List>
+              <List.Item>Windows 11 最新版</List.Item>
+              <List.Item>macOS 14 Sonoma 最新版</List.Item>
+            </List>
+          </Table.Td>
+        </Table.Tr>
+        <Table.Tr>
+          <Table.Th>Webブラウザ</Table.Th>
+          <Table.Td>
+            <List>
+              <List.Item>Google Chrome 最新版</List.Item>
+            </List>
+          </Table.Td>
+        </Table.Tr>
+        <Table.Tr>
+          <Table.Th>解像度</Table.Th>
+          <Table.Td>
+            <List>
+              <List.Item>1280 x 720 以上</List.Item>
+            </List>
+          </Table.Td>
+        </Table.Tr>
+        <Table.Tr>
+          <Table.Td colSpan={2}>
+            これ以外の環境で利用する際に発生する不具合については、サポートの対象外となる場合があります。
+          </Table.Td>
+        </Table.Tr>
+      </Table.Tbody>
+    </Table>
+  );
+};
+
+export default RecommendedEnvironment;
--- src/app/(default)/docs/page.tsx
+++ src/app/(default)/docs/page.tsx
@@ -5,6 +5,7 @@ import { Box, Card, Group, SimpleGrid, Title } from "@mantine/core";
 import { IconArrowRight } from "@tabler/icons-react";
 
 import AppInfo from "./_components/AppInfo";
+import RecommendedEnvironment from "./_components/RecommendedEnvironment";
 
 export const metadata: Metadata = {
   title: "アプリ情報",
@@ -18,7 +19,9 @@ export default function AppInfoPage() {
     <>
       <Title order={2}>アプリ情報</Title>
       <AppInfo />
-      <Title order={3} mt="lg">
+      <Title order={2}>推奨環境</Title>
+      <RecommendedEnvironment />
+      <Title order={2} mt="lg">
         各種表記
       </Title>
       <SimpleGrid cols={2}>
