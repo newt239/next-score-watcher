80022a5192e8d73fec718ce7e763b5696c8936f5
newt <ysti2019.win2020.st@gmail.com>
2025-07-04 19:37:02 +0900

    Revert "chore: add css-module-kit and remove prettier"
    This reverts commit 62594deb3c08eb62236b0473e1eebadf80ee761d.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,49 +1,58 @@
-import antfu from "@antfu/eslint-config";
-import css from "@eslint/css";
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
-export default antfu({
-  css: false,
-  react: true,
-  typescript: true,
-}, {
-  rules: {
-    "style/quotes": ["error", "double"],
-    "style/semi": ["error", "always"],
-    "import/order": [
-      "error",
-      {
-        "groups": [
-          "builtin",
-          "external",
-          "parent",
-          "sibling",
-          "index",
-          "object",
-          "type",
-        ],
-        "pathGroups": [
-          {
-            pattern: "{react,react-dom/**,react-router-dom,next,next/**}",
-            group: "builtin",
-            position: "before",
-          },
-          {
-            pattern: "#/**",
-            group: "parent",
-            position: "before",
-          },
-        ],
-        "pathGroupsExcludedImportTypes": ["builtin", "object"],
-        "alphabetize": {
-          order: "asc",
-        },
-        "newlines-between": "always",
-      },
-    ],
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+
+const eslintConfig = [
+  {
+    ignores: ["node_modules/", ".next/"],
   },
-}, {
-  files: ["**/*.css"],
-  language: "css/css",
-  plugins: { css },
-  extends: ["css/recommended"],
-});
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  eslintConfigPrettier,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "react-hooks/exhaustive-deps": "off",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "caughtErrorsIgnorePattern": "^_",
+          "destructuredArrayIgnorePattern": "^_"
+        }
+      ],
+      "import/order": [
+        "error",
+        {
+          "groups": [
+            "builtin",
+            "external",
+            "parent",
+            "sibling",
+            "index",
+            "object",
+            "type"
+          ],
+          "pathGroups": [
+            {
+              "pattern": "{react,react-dom/**,react-router-dom,next,next/**}",
+              "group": "builtin",
+              "position": "before"
+            },
+          ],
+          "pathGroupsExcludedImportTypes": ["builtin"],
+          "alphabetize": {
+            "order": "asc"
+          },
+          "newlines-between": "always"
+        }
+      ]
+    },
+  },
+];
+
+export default eslintConfig;
